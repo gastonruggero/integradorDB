e3a6ad6f337125a66566ebc58b36f1bbff4942d5
gastonruggero <gastonruggero@gmail.com>
2023-11-08 05:07:33 -0300

    Cambio en la Time Zone
    Estaba en 00:00, lo adapté a -03:00 hora Argentina

--- integrador_cac.sql
+++ integrador_cac.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-11-2023 a las 08:48:05
+-- Tiempo de generación: 08-11-2023 a las 09:06:13
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
